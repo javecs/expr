d4b4007160d646a67db89190d3c367eb6fc5305b
javecs <moonhee.javecs@gmail.com>
2017-04-06 21:14:21 +0900

    足し算を実装しました。

--- src/main/antlr/Expr.g4
+++ src/main/antlr/Expr.g4
@@ -2,8 +2,8 @@ grammar Expr;
 
 start: stat+ ;
 stat: expr NEWLINE? ;
-expr: expr '+' expr
-    | INT
+expr: expr '+' expr     # Add
+    | INT               # int
     ;
 INT: [0-9]+ ;
 NEWLINE: '\r'? '\n' ;
--- src/main/kotlin/xyz/javecs/expr/ExprApplication.kt
+++ src/main/kotlin/xyz/javecs/expr/ExprApplication.kt
@@ -1,23 +1,7 @@
 package xyz.javecs.expr
 
-import org.antlr.v4.runtime.ANTLRInputStream
-import org.antlr.v4.runtime.CommonTokenStream
-import xyz.javecs.expr.parser.ExprLexer
-import xyz.javecs.expr.parser.ExprParser
-
-fun parser(expr: String): ExprParser {
-    val input = ANTLRInputStream(expr.byteInputStream())
-    val lexer = ExprLexer(input)
-    val tokens = CommonTokenStream(lexer)
-    return ExprParser(tokens)
-}
-
-fun parserTree(expr: String): String {
-    val parser = parser(expr)
-    val tree = parser.start()
-    return tree.toStringTree(parser)
-}
-
 fun main(args: Array<String>) {
-    println(parserTree("3 + 4"))
+    val expr = "3 + 4";
+    println(parserTree(expr))
+    println(eval(expr))
 }

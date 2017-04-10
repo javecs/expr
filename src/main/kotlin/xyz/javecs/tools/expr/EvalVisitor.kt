fceaedc040d7de2ba227518bd7d35b31d82f228a
javecs <moonhee.javecs@gmail.com>
2017-04-11 01:34:00 +0900

    EvalVisitorをクラスファイルに分けました。

---
+++ src/main/kotlin/xyz/javecs/tools/expr/EvalVisitor.kt
@@ -0,0 +1,17 @@
+package xyz.javecs.tools.expr
+
+import xyz.javecs.tools.expr.parser.ExprBaseVisitor
+import xyz.javecs.tools.expr.parser.ExprParser
+
+class EvalVisitor : ExprBaseVisitor<Double>() {
+    override fun visitParens(ctx: ExprParser.ParensContext?) = visit(ctx!!.expr())!!
+    override fun visitNumber(ctx: ExprParser.NumberContext?) = ctx!!.NUMBER().text.toDouble()
+    override fun visitAdd(ctx: ExprParser.AddContext?) = visit(ctx!!.expr(0)) + visit(ctx.expr(1))
+    override fun visitSub(ctx: ExprParser.SubContext?) = visit(ctx!!.expr(0)) - visit(ctx.expr(1))
+    override fun visitMul(ctx: ExprParser.MulContext?) = visit(ctx!!.expr(0)) * visit(ctx.expr(1))
+    override fun visitDiv(ctx: ExprParser.DivContext?): Double {
+        val left = visit(ctx!!.expr(0))
+        val right = visit(ctx.expr(1))
+        return if (right == 0.0) Double.NaN else left / right
+    }
+}

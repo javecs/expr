0e8e6f5e0b1a1ed50aa994ef9528f9f9dac82234
javecs <moonhee.javecs@gmail.com>
2017-04-18 07:48:15 +0900

    公開しないモジュールを隠しました。

--- src/main/kotlin/xyz/javecs/tools/expr/EvalContext.kt
+++ src/main/kotlin/xyz/javecs/tools/expr/EvalContext.kt
@@ -1,6 +1,6 @@
 package xyz.javecs.tools.expr
 
-interface EvalContext {
+internal interface EvalContext {
     fun getSymbol(name: String): Double
     fun putSymbol(name:String, value: Double)
 }
--- src/main/kotlin/xyz/javecs/tools/expr/Evaluator.kt
+++ src/main/kotlin/xyz/javecs/tools/expr/Evaluator.kt
@@ -7,14 +7,14 @@ import org.antlr.v4.runtime.CommonTokenStream
 import xyz.javecs.tools.expr.parser.ExprLexer
 import xyz.javecs.tools.expr.parser.ExprParser
 
-fun parser(expr: String): ExprParser {
+internal fun parser(expr: String): ExprParser {
     val input = ANTLRInputStream(expr.byteInputStream())
     val lexer = ExprLexer(input)
     val tokens = CommonTokenStream(lexer)
     return ExprParser(tokens)
 }
 
-fun parserTree(expr: String): String {
+internal fun parserTree(expr: String): String {
     val parser = parser(expr)
     val tree = parser.start()
     return tree.toStringTree(parser)

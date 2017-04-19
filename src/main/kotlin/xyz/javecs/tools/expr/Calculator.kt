afd0904798e2514e7eef2d81038dec97cdf13973
javecs <moonhee.javecs@gmail.com>
2017-04-19 22:52:13 +0900

    コードを見直しました。

--- src/main/kotlin/xyz/javecs/tools/expr/Calculator.kt
+++ src/main/kotlin/xyz/javecs/tools/expr/Calculator.kt
@@ -4,9 +4,9 @@ import kotlin.collections.HashMap
 
 class Calculator(expressions: Array<String> = emptyArray()) : EvalContext {
     private val evaluator = EvalVisitor(this)
-    private val symbol: HashMap<String, Double> = HashMap()
-    private val constant = constant()
-    private val function = builtIn()
+    private val variables = HashMap<String, Double>()
+    private val constants = constant()
+    private val functions = builtIn()
     private var expression = Expression()
     var value: Number = expression.value
         get() = expression.getValue()
@@ -30,22 +30,19 @@ class Calculator(expressions: Array<String> = emptyArray()) : EvalContext {
     }
 
     fun clear() {
-        symbol.clear()
+        variables.clear()
         expression = Expression()
     }
 
-    override fun isConstant(name: String) = constant.contains(name.toLowerCase())
-    override fun get(name: String) = constant.getOrDefault(name.toLowerCase(), symbol.getOrDefault(name, Double.NaN))
+    override fun toString() = expression.toString()
+    override fun isConstant(name: String) = constants.contains(name.toLowerCase())
+    override fun call(name: String, value: Double) = functions.getOrDefault(name.toLowerCase(), { Double.NaN })(value)
+    override fun get(name: String) = constants.getOrDefault(name.toLowerCase(), variables.getOrDefault(name, Double.NaN))
     override fun put(name:String, value: Double) = when (isConstant(name)) {
         true -> Double.NaN
         else -> {
-            symbol[name] = value
+            variables[name] = value
             value
         }
-    }
-    override fun call(name: String, value: Double) = function.getOrDefault(name.toLowerCase(), { Double.NaN })(value)
-
-    override fun toString(): String {
-        return expression.toString()
     }
 }

3b78baf0591cd3449d1db5993e83f53ae88a5fbb
javecs <moonhee.javecs@gmail.com>
2017-04-21 22:25:27 +0900

    関数のシグネチャーから、名前と引く数を取得するように修正しました。

--- src/main/kotlin/xyz/javecs/tools/expr/Function.kt
+++ src/main/kotlin/xyz/javecs/tools/expr/Function.kt
@@ -1,22 +1,26 @@
 package xyz.javecs.tools.expr
 
-class Function(val name: String, val params: Array<String>) {
+import kotlin.streams.toList
+
+private val sPattern = Regex("[a-zA-Z][a-zA-Z0-9]*\\s*\\(\\s*([a-zA-Z][a-zA-Z0-9]*\\s*(,\\s*[a-zA-Z][a-zA-Z0-9]*)*)?\\s*\\)")
+internal fun validate(signature: String): Pair<String, Array<String>> {
+    if (!sPattern.matches(signature.trim())) throw Exception()
+    val separated = signature.replace(" ", "").split("(", ",", ")").stream().filter { !it.isBlank() }.toList()
+    return Pair(separated[0], if (separated.size > 1) {
+        separated.subList(1, separated.size).toTypedArray()
+    } else {
+        emptyArray()
+    })
+}
+
+class Function(signature: String, val codes: Array<String>) {
     private val calc = Calculator()
-    private val codes = ArrayList<String>()
-
-    fun code(code: String) {
-        codes.add(code)
-    }
-
+    val name = validate(signature).first
+    val params = validate(signature).second
     fun call(args: Array<Double>): Double {
-        if (params.size != args.size) {
-            return Double.NaN
-        }
-
+        if (params.size != args.size) return Double.NaN
         calc.clear()
-        for (i in params.indices) {
-            calc.eval("${params[i]} = ${args[i]}")
-        }
+        params.indices.forEach( { calc.eval("${params[it]} = ${args[it]}") })
         codes.forEach( { calc.eval(it) })
         return calc.value.toDouble()
     }

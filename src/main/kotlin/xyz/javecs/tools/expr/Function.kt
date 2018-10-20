40aeb6a380361b0d545a657ea073cb8d874bebae
javecs <moonhee.javecs@gmail.com>
2018-10-20 00:19:32 -0700

    update kotlin and gradle version

--- src/main/kotlin/xyz/javecs/tools/expr/Function.kt
+++ src/main/kotlin/xyz/javecs/tools/expr/Function.kt
@@ -1,11 +1,14 @@
 package xyz.javecs.tools.expr
 
-import kotlin.streams.toList
-
 private val sPattern = Regex("[a-zA-Z][a-zA-Z0-9]*\\s*\\(\\s*([a-zA-Z][a-zA-Z0-9]*\\s*(,\\s*[a-zA-Z][a-zA-Z0-9]*)*)?\\s*\\)")
 internal fun validate(signature: String): Pair<String, Array<String>> {
     if (!sPattern.matches(signature.trim())) throw Exception()
-    val separated = signature.replace(" ", "").split("(", ",", ")").stream().filter { !it.isBlank() }.toList()
+    val separated = signature
+            .replace(" ", "")
+            .split("(", ",", ")")
+            .asSequence()
+            .filter { !it.isBlank() }
+            .toList()
     return Pair(separated[0], if (separated.size > 1) {
         separated.subList(1, separated.size).toTypedArray()
     } else {
@@ -23,8 +26,8 @@ class Function(signature: String, val codes: Array<String>) {
     fun call(args: Array<Double>): Double {
         if (params.size != args.size) return Double.NaN
         calc.clear()
-        params.indices.forEach( { calc.eval("${params[it]} = ${args[it]}") })
-        codes.forEach( { calc.eval(it) })
+        params.indices.forEach { calc.eval("${params[it]} = ${args[it]}") }
+        codes.forEach { calc.eval(it) }
         return calc.value.toDouble()
     }
 }
